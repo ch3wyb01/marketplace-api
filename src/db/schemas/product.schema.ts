30092afe138a7d1c6e10bb418d24022d96fbd72c
Becca <beccasullivan30@gmail.com>
2022-02-26 18:45:42 +0000

    rename ProductDocument to Product

--- src/db/schemas/product.schema.ts
+++ src/db/schemas/product.schema.ts
@@ -1,10 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-export type ProductDocument = Product & Document;
+export type Product = _productSchema & Document;
 
 @Schema()
-export class Product {
+class _productSchema {
   @Prop({ required: true })
   title: string;
 
@@ -15,4 +15,4 @@ export class Product {
   price: number;
 }
 
-export const ProductSchema = SchemaFactory.createForClass(Product);
+export const ProductSchema = SchemaFactory.createForClass(_productSchema);
