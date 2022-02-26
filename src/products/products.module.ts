db4eee7b38012743252c40bca04edb1d85753e9e
Becca <beccasullivan30@gmail.com>
2022-02-26 17:02:37 +0000

    import product schema and model

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { Product, ProductSchema } from '../db/schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
+
 export class ProductsModule {}
