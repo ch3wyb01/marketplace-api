a74d9cbc60628a51d35df7ffb9f25e91a2af165a
Becca <beccasullivan30@gmail.com>
2022-02-28 11:21:48 +0000

    create product test data

---
+++ src/db/data/test-data/products.ts
@@ -0,0 +1,19 @@
+import { IProduct } from "src/products/products.interface";
+
+export const products: IProduct[] = [
+  {
+    title: 'Vintage Book Case',
+    description: 'A mahogany book case from the 1920s',
+    price: 400,
+  },
+  {
+    title: 'Supersized Bean Bag',
+    description: 'A big fluffy bean bag',
+    price: 80,
+  },
+  {
+    title: 'Watercolour Painting',
+    description: 'A lovely painting of a lake',
+    price: 30,
+  },
+];
