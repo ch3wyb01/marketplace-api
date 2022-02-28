1eed3c4f962763c25bb6d532289df7985b7fea90
Becca <beccasullivan30@gmail.com>
2022-02-28 11:21:31 +0000

    schema implements interface

--- src/db/schemas/product.schema.ts
+++ src/db/schemas/product.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { IProduct } from 'src/products/products.interface';
 
 export type Product = _productSchema & Document;
 
 @Schema()
-class _productSchema {
+class _productSchema implements IProduct{
   @Prop({ required: true })
   title: string;
 
