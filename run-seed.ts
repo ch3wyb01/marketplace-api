8390524917fda65ebf61f50f0ef367ec6af6d503
Becca <beccasullivan30@gmail.com>
2022-03-02 15:25:13 +0000

    create seed for development

---
+++ run-seed.ts
@@ -0,0 +1,18 @@
+import developmentData from './src/db/data/development-data';
+import mongoose from 'mongoose';
+import seed from './seed';
+
+const ENV = process.env.NODE_ENV || 'development';
+
+require('dotenv').config({
+  path: `${__dirname}/${ENV}.env`,
+});
+
+const runSeed = async () => {
+  const mongo = await mongoose.connect(process.env.DB_URI);
+  await seed(mongo.connection, developmentData);
+  await mongo.connection.close();
+  await mongo.disconnect();
+};
+
+runSeed();
