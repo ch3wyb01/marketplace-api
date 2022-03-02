2cee1fbcb4d8dbc450cca0506fe0c3a9f5b7ff6a
Becca <beccasullivan30@gmail.com>
2022-03-02 12:46:03 +0000

    seed database with test data between tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import testData from '../src/db/data/test-data/index';
+import { SeedDatabaseService } from '../src/db/seeds/seed.service';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+let app: INestApplication;
+let seedService: SeedDatabaseService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+beforeEach(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+    providers: [SeedDatabaseService]
+  }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  app = moduleFixture.createNestApplication();
+  await app.init();
 
+  seedService = app.get(SeedDatabaseService);
+  await seedService.SeedDatabase(testData);
+});
+
+afterAll(async () => {
+  await seedService.CloseConnection();
+});
+
+describe('Tests', () => {
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/products').expect(200);
   });
 });
