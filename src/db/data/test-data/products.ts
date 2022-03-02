59984cfb537b5b0ef90db99612fcd52b5b315fd4
Becca <beccasullivan30@gmail.com>
2022-03-02 16:21:45 +0000

    add IDs to test data

--- src/db/data/test-data/products.ts
+++ src/db/data/test-data/products.ts
@@ -1,17 +1,20 @@
-import { IProduct } from "src/products/product.interface";
+import mongoose from 'mongoose';
 
-const productsData: IProduct[] = [
+const productsData = [
   {
+    _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f0'),
     title: 'Abacus',
     description: 'A wooden abacus that will help teach counting skills',
     price: 10,
   },
   {
+    _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f1'),
     title: 'Blue Whale Plush',
     description: 'A small whale teddy',
     price: 8,
   },
   {
+    _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f2'),
     title: 'Crystal Wine Glasses',
     description: 'A set of 4 wine glasses',
     price: 30,
