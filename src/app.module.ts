ecb61907245dd46aa95371652168bb080ae60b88
Becca <beccasullivan30@gmail.com>
2022-03-02 12:43:29 +0000

    import database and seed service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SeedDatabaseService } from './db/seeds/seed.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.env.NODE_ENV || 'development'}.env`,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        return <MongooseModuleOptions>{
+          uri: config.get<string>('DB_URI'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SeedDatabaseService],
 })
 export class AppModule {}
