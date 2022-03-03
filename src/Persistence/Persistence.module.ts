cbc799ccaf923a6d12088297a3d25d96bb3be9b0
Becca <beccasullivan30@gmail.com>
2022-03-03 16:54:34 +0000

    import/export categories repository module

--- src/Persistence/Persistence.module.ts
+++ src/Persistence/Persistence.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CategoriesRepositoryModule } from './categories/CategoriesRepository.module';
 import { ProductsRepositoryModule } from './products/ProductsRepository.module';
 
 @Module({
-  imports: [ProductsRepositoryModule],
-  exports: [ProductsRepositoryModule],
+  imports: [ProductsRepositoryModule, CategoriesRepositoryModule],
+  exports: [ProductsRepositoryModule, CategoriesRepositoryModule],
 })
 export class PersistenceModule {}
