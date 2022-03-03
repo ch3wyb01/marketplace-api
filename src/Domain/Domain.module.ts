dfb4f5a249bea5df5dcbc3d2e1bf81fd10169e0d
Becca <beccasullivan30@gmail.com>
2022-03-03 14:17:16 +0000

    add domain module imports and exports

--- src/Domain/Domain.module.ts
+++ src/Domain/Domain.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PersistenceModule } from 'src/Persistence/Persistence.Module';
 import { UtilitiesModule } from 'src/Utilities/Utilities.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PersistenceModule, UtilitiesModule],
+  imports: [ProductsModule, PersistenceModule, UtilitiesModule],
+  providers: [],
+  exports: [ProductsModule]
 })
 export class DomainModule {}
