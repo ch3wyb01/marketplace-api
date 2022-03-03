557150a2d76ed2730d3dc80bfbf11c4a48ec12c9
Becca <beccasullivan30@gmail.com>
2022-03-03 16:43:56 +0000

    import categories controller

--- src/API/API.module.ts
+++ src/API/API.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DomainModule } from '../Domain/Domain.module';
 import { UtilitiesModule } from '../Utilities/Utilities.module';
+import { CategoriesController } from './categories/categories.controller';
 import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [DomainModule, UtilitiesModule],
-  controllers: [ProductsController],
+  controllers: [ProductsController, CategoriesController],
 })
 export class APIModule {}
