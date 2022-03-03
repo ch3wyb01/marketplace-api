68076a90cad76d153be759266a2853eb913c5759
Becca <beccasullivan30@gmail.com>
2022-03-03 14:42:36 +0000

    import seed service

--- src/Utilities/Utilities.module.ts
+++ src/Utilities/Utilities.module.ts
@@ -1,1 +1,8 @@
-export class UtilitiesModule{}
+import { Module } from '@nestjs/common';
+import { SeedDatabaseService } from './seeds/seed.service';
+
+@Module({
+  providers: [SeedDatabaseService],
+  exports: [SeedDatabaseService],
+})
+export class UtilitiesModule {}
