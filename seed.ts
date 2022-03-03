ae6853666a241eaabec13f2ff08839c7e3b2dcca
Becca <beccasullivan30@gmail.com>
2022-03-03 18:50:34 +0000

    add categories to seed function

--- seed.ts
+++ seed.ts
@@ -1,9 +1,19 @@
-const seed = async (connection, { productsData }) => {
-  await connection.dropCollection('products');
+import { Connection } from 'mongoose';
+import { CategorySchema } from './src/Persistence/categories/category.schema';
+import { ProductSchema } from './src/Persistence/products/product.schema';
 
-  const productCollection = await connection.createCollection('products');
+const seed = async (
+  connection: Connection,
+  { categoriesData, productsData },
+) => {
+  const categoryCollection = connection.model('categories', CategorySchema);
+  await categoryCollection.deleteMany();
+  await categoryCollection.insertMany(categoriesData);
 
+  const productCollection = connection.model('products', ProductSchema);
+  await productCollection.deleteMany();
   await productCollection.insertMany(productsData);
+
 };
 
 export default seed;
