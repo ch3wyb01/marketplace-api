3c41653e3cfb4a8134d3c11ddda466f0edec3e0c
Becca <beccasullivan30@gmail.com>
2022-03-03 16:24:52 +0000

    create categories service

---
+++ src/Domain/categories/categories.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { CategoriesRepository } from 'src/Persistence/categories/categories.repository';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly categoriesRepository: CategoriesRepository) {}
+
+  async fetchAllCategories() {
+    const categories = await this.categoriesRepository.fetchAllCategories();
+    return categories;
+  }
+
+  async insertCategory(body: {
+    category_name: string;
+    category_description: string;
+  }) {
+    const category = await this.categoriesRepository.insertCategory(body);
+    return category;
+  }
+}
