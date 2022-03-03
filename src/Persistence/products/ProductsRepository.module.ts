1a5549e2e56f31368088616c3947f031a4ceb4b3
Becca <beccasullivan30@gmail.com>
2022-03-03 14:42:57 +0000

    provide and export repository

--- src/Persistence/products/ProductsRepository.module.ts
+++ src/Persistence/products/ProductsRepository.module.ts
@@ -1,9 +1,13 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ProductSchema } from "./product.schema";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './product.schema';
+import { ProductsRepository } from './products.repository';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }])]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
+  providers: [ProductsRepository],
+  exports: [ProductsRepository],
 })
-
 export class ProductsRepositoryModule {}
