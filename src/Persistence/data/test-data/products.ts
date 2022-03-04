f8ce667465858809c3d03e378b13e3274bb91254
Becca <beccasullivan30@gmail.com>
2022-03-04 15:16:38 +0000

    add categories

--- src/Persistence/data/test-data/products.ts
+++ src/Persistence/data/test-data/products.ts
@@ -6,18 +6,21 @@ const productsData = [
     title: 'Abacus',
     description: 'A wooden abacus that will help teach counting skills',
     price: 10,
+    categories: ['6220f9ab230ed15af3d3dffa', '6220f9ab230ed15af3d3dffb'],
   },
   {
     _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f1'),
     title: 'Blue Whale Plush',
     description: 'A small whale teddy',
     price: 8,
+    categories: ['6220f9ab230ed15af3d3dffb'],
   },
   {
     _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f2'),
     title: 'Crystal Wine Glasses',
     description: 'A set of 4 wine glasses',
     price: 30,
+    categories: ['6220f9ab230ed15af3d3dffc'],
   },
 ];
 
