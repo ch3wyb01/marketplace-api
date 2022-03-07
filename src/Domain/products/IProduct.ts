3707d21e41e438f9e2d03431894fae9f13592b8b
Becca <beccasullivan30@gmail.com>
2022-03-07 15:40:08 +0000

    remove imports

--- src/Domain/products/IProduct.ts
+++ src/Domain/products/IProduct.ts
@@ -1,8 +1,5 @@
-import mongoose from "mongoose";
-import { Category } from "src/Persistence/categories/category.schema";
-
 export interface IProduct {
-  _id?: string | null,
+  _id?: string | null;
   title: string;
   description: string;
   price: number;
