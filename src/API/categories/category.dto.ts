326ae7824d46359bb87f77461e576705017e24f6
Becca <beccasullivan30@gmail.com>
2022-03-07 15:40:49 +0000

    change DTO to include ID

--- src/API/categories/category.dto.ts
+++ src/API/categories/category.dto.ts
@@ -1,8 +1,10 @@
 export class CategoryDTO {
+  id: string;
   category_name: string;
   category_description: string;
 
-  constructor(category_name: string, category_description: string) {
+  constructor(id: string, category_name: string, category_description: string) {
+    this.id = id;
     this.category_name = category_name;
     this.category_description = category_description;
   }
