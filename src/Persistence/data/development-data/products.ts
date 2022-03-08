82b35b5b7e8612b45438b690454e0d4fde27f747
Becca <beccasullivan30@gmail.com>
2022-03-08 13:15:06 +0000

    add images to product data

--- src/Persistence/data/development-data/products.ts
+++ src/Persistence/data/development-data/products.ts
@@ -3,20 +3,26 @@ import { IProduct } from '../../../Domain/products/IProduct';
 const productsData: IProduct[] = [
   {
     title: 'Vintage Book Case',
-    description: 'A mahogany book case from the 1920s',
+    description: 'A mahogany book case from the 1960s',
+    img_url:
+      'https://images.unsplash.com/photo-1603745676022-d1b77e3cbce8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80',
     price: 400,
     categories: ['62274a4b1b3b71dc83f59cb7'],
   },
   {
-    title: 'Supersized Bean Bag',
-    description: 'A big fluffy bean bag',
-    price: 80,
+    title: 'Burnt peach armchair',
+    description: 'The perfect chair for reading',
+    img_url:
+      'https://images.unsplash.com/photo-1599697686548-d39f7c80174c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80',
+    price: 120,
     categories: ['62274a4b1b3b71dc83f59cb7'],
   },
   {
-    title: 'Watercolour Painting',
-    description: 'A lovely painting of a lake',
-    price: 30,
+    title: 'Watercolour Painting of Greenfield House',
+    description: 'A lovely painting by David Cox Jnr',
+    img_url:
+      'https://images.unsplash.com/photo-1578926372062-6ddf6fc55e0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=912&q=80',
+    price: 200,
     categories: ['62274a4b1b3b71dc83f59cb8'],
   },
 ];
--- src/Persistence/data/test-data/products.ts
+++ src/Persistence/data/test-data/products.ts
@@ -5,6 +5,8 @@ const productsData = [
     _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f0'),
     title: 'Abacus',
     description: 'A wooden abacus that will help teach counting skills',
+    img_url:
+      'https://images.unsplash.com/photo-1548690596-f1722c190938?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80',
     price: 10,
     categories: ['6220f9ab230ed15af3d3dffa', '6220f9ab230ed15af3d3dffb'],
   },
@@ -12,6 +14,8 @@ const productsData = [
     _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f1'),
     title: 'Blue Whale Plush',
     description: 'A small whale teddy',
+    img_url:
+      'https://images.unsplash.com/photo-1584155828260-3791b07e6afb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=865&q=80',
     price: 8,
     categories: ['6220f9ab230ed15af3d3dffb'],
   },
@@ -19,6 +23,8 @@ const productsData = [
     _id: new mongoose.Types.ObjectId('621f912430f443d5067c39f2'),
     title: 'Crystal Wine Glasses',
     description: 'A set of 4 wine glasses',
+    img_url:
+      'https://images.unsplash.com/photo-1598306442928-4d90f32c6866?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
     price: 30,
     categories: ['6220f9ab230ed15af3d3dffc'],
   },
