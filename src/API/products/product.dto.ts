8b2294d00ac191ba9467bbf8fc827c0dc94570e1
Becca <beccasullivan30@gmail.com>
2022-03-15 22:49:59 +0000

    add class validator decorators to DTO

--- src/API/products/product.dto.ts
+++ src/API/products/product.dto.ts
@@ -1,10 +1,29 @@
+import { IsNotEmpty, IsString, IsNumber, IsArray } from 'class-validator';
+
 export class ProductDTO {
-  id: string;
-  title: string;
-  description: string;
-  img_url: string;
-  price: number;
-  categories: string[];
+  @IsNotEmpty()
+  @IsString()
+  public id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  public img_url: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  public price: number;
+
+  @IsNotEmpty()
+  @IsArray()
+  public categories: string[];
 
   constructor(
     id: string,
