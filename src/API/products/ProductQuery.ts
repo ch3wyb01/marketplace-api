19c7e1f5400bd443c3d8558cae568e30805560db
Becca <beccasullivan30@gmail.com>
2022-03-21 11:18:01 +0000

    change to class and add validation

--- src/API/products/ProductQuery.ts
+++ src/API/products/ProductQuery.ts
@@ -1,9 +1,22 @@
-export type ProductQuery = {
-  title?: string;
+import { IsOptional, IsString, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
+export class ProductQuery {
+  @IsOptional()
+  @IsString()
+  public title?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
   priceMin?: number;
 
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
   priceMax?: number;
 
+  @IsOptional()
+  @IsString()
   category?: string;
-};
+}
