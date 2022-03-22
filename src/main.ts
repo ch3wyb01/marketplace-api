f9d418b28fc1c9cadfda9f2b8b652f44ca2854c8
Becca <beccasullivan30@gmail.com>
2022-03-22 15:10:52 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { validationPipeOptions } from './Utilities/exceptionHandler/validation/v
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
