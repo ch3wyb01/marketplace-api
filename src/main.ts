5e866588e41c23d15a89997aeefc754d3ccbd861
Becca <beccasullivan30@gmail.com>
2022-03-22 14:43:57 +0000

    add port to listen

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { validationPipeOptions } from './Utilities/exceptionHandler/validation/v
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
